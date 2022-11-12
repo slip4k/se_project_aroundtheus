5c398d04016dc2ce5e99237be75cbca05fcd640c
slip4k <justinjaeshim@gmail.com>
2022-11-12 13:59:04 -0800

    Add Scripts

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+    const object 1 = {
+        name: 'Yosemite Valley',
+        link: '../images/yosemite-valley.png',
+    }
+    const object 2 = {
+        name: 'Lake Louise',
+        link: '../images/lake-louise.png'
+    }
+    const object 3 = {
+        name: 'Bald Mountains',
+        link: '../images/bald-mountains.png'
+    }
+    const object 4 = {
+        name: 'Latemar',
+        link: '../images/latemar.png'
+    }
+    const object 5 = {
+        name: 'Vanoise National Park',
+        link: '../images/vanoise-national-park.png'
+    }
+    const object 6 = {
+        name: 'Lago Di Braies',
+        link: '../images/lago-di-braies.png'
+    }
+]
