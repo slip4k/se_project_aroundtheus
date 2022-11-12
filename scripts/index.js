c1a1e3fc6469f3ccca26db169790c3d053cd634f
slip4k <justinjaeshim@gmail.com>
2022-11-12 14:08:19 -0800

    Update Scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,25 @@
-let initialCards = [
-    const object 1 = {
-        name: 'Yosemite Valley',
-        link: '../images/yosemite-valley.png',
-    }
-    const object 2 = {
-        name: 'Lake Louise',
-        link: '../images/lake-louise.png'
-    }
-    const object 3 = {
-        name: 'Bald Mountains',
-        link: '../images/bald-mountains.png'
-    }
-    const object 4 = {
-        name: 'Latemar',
-        link: '../images/latemar.png'
-    }
-    const object 5 = {
-        name: 'Vanoise National Park',
-        link: '../images/vanoise-national-park.png'
-    }
-    const object 6 = {
-        name: 'Lago Di Braies',
-        link: '../images/lago-di-braies.png'
-    }
-]
+const card1 = {
+  name: "Yosemite Valley",
+  link: "./images/yosemite-valley.png",
+};
+const card2 = {
+  name: "Lake Louise",
+  link: "./images/lake-louise.png",
+};
+const card3 = {
+  name: "Bald Mountains",
+  link: "./images/bald-mountains.png",
+};
+const card4 = {
+  name: "Latemar",
+  link: "./images/latemar.png",
+};
+const card5 = {
+  name: "Vanoise National Park",
+  link: "./images/vanoise-national-park.png",
+};
+const card6 = {
+  name: "Lago Di Braies",
+  link: "./images/lago-di-braies.png",
+};
+const initialCards = [card1, card2, card3, card4, card5, card6];
